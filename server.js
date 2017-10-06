c8e2da55309a2898b806c6080d8bd9e8410eaaf6
Troy Amelotte <troy.amelotte@gTAef49b1.local>
2017-10-06 08:49:27 -0700

    initpt2

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var path = require("path");
 var app = express();
 var bodyParser = require('body-parser');
+var fs = require('fs');
 var port = process.env.PORT || 8000;
 app.use(bodyParser.json({ extended: true }));
 
@@ -10,29 +11,92 @@ app.use(bodyParser.json({ extended: true }));
 
 // Get all users
 app.get('/users', function(req, res) {
+  fs.readFile('./storage.json', 'utf8', function(err, data){
+    if(err) throw err;
+    let usersArr = JSON.parse(data);
 
+    res.json(usersArr);
+  })
 });
 
 // Get one user
-app.get('/users/:id', function(req, res) {
+app.get('/users/:name', function(req, res) {
+  fs.readFile('./storage.json', 'utf8', function(err, data){
+    if(err) throw err;
+    let usersArr = JSON.parse(data);
 
+    for(let i = 0; i<usersArr.length; i++){
+      if(usersArr[i].name == req.params.name){
+        res.json(usersArr[i]);
+        return;
+      }
+    }
+    res.sendStatus(400);
+  })
 });
 
 // Create new user
 app.post('/users', function(req, res) {
+  fs.readFile("./storage.json", "utf8", function(err, data){
+    if(err) throw err;
+    let usersArr = JSON.parse(data);
 
+    console.log(req.body);
+    usersArr.push(req.body);
+
+    fs.writeFile("./storage.json", JSON.stringify(usersArr), function(err){
+      if(err) throw err;
+
+      res.sendStatus(200);
+
+    })
+
+  })
 });
 
 // Update one user
-app.put('/users/:id', function(req, res) {
+app.put('/users/:name', function(req, res) {
+  fs.readFile('./storage.json', 'utf8', function(err, data){
+    if(err) throw err;
+    let usersArr = JSON.parse(data);
 
+    for(let i = 0; i<usersArr.length; i++){
+      if(usersArr[i].name == req.params.name){
+        usersArr[i] = req.body;
+
+        fs.writeFile('./storage.json', JSON.stringify(usersArr), function(err){
+          console.log("success!");
+          res.sendStatus(200);
+        });
+        return;
+
+      }
+    }
+    res.sendStatus(400);
+  })
 });
 
 // Delete one user
-app.delete('/users/:id', function(req, res) {
-  console.log("Making a delete request");
+app.delete('/users/:name', function(req, res) {
+  fs.readFile('./storage.json', 'utf8', function(err, data){
+    if(err) throw err;
+    let usersArr = JSON.parse(data);
 
-  res.json(req.params.id);
+    for(let i = 0; i<usersArr.length; i++){
+      if(usersArr[i].name == req.params.name){
+
+        usersArr.splice(i, 1);
+
+        fs.writeFile('./storage.json', JSON.stringify(usersArr), function(err){
+          console.log("success!");
+          res.sendStatus(200);
+        });
+        return;
+
+      }
+    }
+    res.sendStatus(400);
+  })
 });
 
 app.listen(port, function() {
