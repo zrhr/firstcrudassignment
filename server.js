5f5ff24c7d5586dd9e1ede7fc222b9a79531b3ba
Troy Amelotte <troyamelotte@gmail.com>
2018-12-07 12:20:28 -0700

    assignment fix

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 8000;
+// Use the array below to store the users. Add/remove/update items in it based off
+let storage = [];
+app.use(bodyParser.json());
+
+
+app.listen(port, ()=>{
+  console.log(`Listening on port ${port}`);
+})
